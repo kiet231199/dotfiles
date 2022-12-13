dc14c78a7d78602e2019e5f5a1c1d5700802349e
kiet231199 <phamanhkiet231199@gmail.com>
2022-12-13 22:42:29 +0700

    Update command

--- script/command.sh
+++ script/command.sh
@@ -6,3 +6,8 @@ git config --local core.autocrlf false
 
 # Convert dos 2 unix for all files
 find . -type f -print0 | xargs -0 dos2unix
+
+# Search all *.c files in all subdirectories
+dirname $(find -name '*.c' -o -name '*.h') | sort | awk '!a[$0]++'
+dirname $(find -name '*.c') | sort | awk '!a[$0]++' | awk '{print $0"/*.c"}'
+dirname $(find -name '*.h') | sort | awk '!a[$0]++' | awk '{print $0"/*.h"}'
